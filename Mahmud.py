073086ed924dd7a1e6a86427bbb3de23a270f700
unknown <isaalimahmud02@gmail.com>
2021-07-18 19:33:23 +0400

    nigar

---
+++ Mahmud.py
@@ -0,0 +1,12 @@
+b=""
+for i in range(-1, -1,-1):
+    b=b+i
+for z in range(len(i)-1, 1,1):
+    b=b+z
+for x in range(1,1001,i[a==b]:
+               
+               
+                b=b+x
+print(b)
+                   
+               
